5b3435a3f973bd4d5dd5024846ef610dbfe3fc15
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-12-30 20:05:04 +0100

    sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -30,24 +30,42 @@ mix.webpackConfig({
     }
 });
 
-mix.js('resources/js/vendor/icore/vendor/vendor.js', 'js/vendor/icore/vendor')
-    .js('resources/js/vendor/icore/web/web.js', 'js/vendor/icore/web')
-    .js('resources/js/vendor/icore/admin/admin.js', 'js/vendor/icore/admin')
-    .scripts([
-        'resources/js/vendor/icore/web/scripts/**/*.js'
+// Custom assets
+mix.js('resources/js/web/web.js', 'public/js/custom/web')
+    .js('resources/js/admin/admin.js', 'public/js/custom/admin')
+    .babel([
+        'resources/js/web/scripts/**/*.js'
+    ], 'public/js/custom/web/scripts.js')
+    .babel([
+        'resources/js/admin/scripts/**/*.js'
+    ], 'public/js/custom/admin/scripts.js')
+    .sass('resources/sass/web/web.scss',
+        'public/css/custom/web')
+    .sass('resources/sass/web/web-dark.scss',
+        'public/css/custom/web')        
+    .sass('resources/sass/admin/admin.scss',
+        'public/css/custom/admin')
+    .sass('resources/sass/admin/admin-dark.scss',
+        'public/css/custom/admin')
+    // iCore assets
+    .js('vendor/n1ebieski/icore/resources/js/vendor/vendor.js', 'js/vendor/icore/vendor')
+    .js('vendor/n1ebieski/icore/resources/js/web/web.js', 'js/vendor/icore/web')
+    .js('vendor/n1ebieski/icore/resources/js/admin/admin.js', 'js/vendor/icore/admin')
+    .babel([
+        'vendor/n1ebieski/icore/resources/js/web/scripts/**/*.js'
     ], 'public/js/vendor/icore/web/scripts.js')
-    .scripts([
-        'resources/js/vendor/icore/admin/scripts/**/*.js'
-    ], 'public/js/vendor/icore/admin/scripts.js')
-    .sass('resources/sass/vendor/icore/vendor/vendor.scss',
+    .babel([
+        'vendor/n1ebieski/icore/resources/js/admin/scripts/**/*.js'
+    ], 'public/js/vendor/icore/admin/scripts.js') 
+    .sass('vendor/n1ebieski/icore/resources/sass/vendor/vendor.scss',
         'css/vendor/icore/vendor')
-    .sass('resources/sass/vendor/icore/web/web.scss',
+    .sass('vendor/n1ebieski/icore/resources/sass/web/web.scss',
         'css/vendor/icore/web')
-    .sass('resources/sass/vendor/icore/web/web-dark.scss',
+    .sass('vendor/n1ebieski/icore/resources/sass/web/web-dark.scss',
         'css/vendor/icore/web')
-    .sass('resources/sass/vendor/icore/admin/admin.scss',
+    .sass('vendor/n1ebieski/icore/resources/sass/admin/admin.scss',
         'css/vendor/icore/admin')
-    .sass('resources/sass/vendor/icore/admin/admin-dark.scss',
+    .sass('vendor/n1ebieski/icore/resources/sass/admin/admin-dark.scss',
         'css/vendor/icore/admin')
     .browserSync({
         proxy: 'https://localhost',
