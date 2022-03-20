9858b77474639e1085ffcf5f0b2f05e9873608a8
N1ebieski <mariusz.wysokinski@neostrada.pl>
2022-03-20 14:07:51 +0100

    6.0

--- config/services.php
+++ config/services.php
@@ -45,6 +45,11 @@ return [
         'site_key'   => env('RECAPTCHA_V2_SITE_KEY'),
     ],
 
+    'recaptcha_invisible' => [
+        'secret_key' => env('RECAPTCHA_INVISIBLE_SECRET_KEY'),
+        'site_key'   => env('RECAPTCHA_INVISIBLE_SITE_KEY'),
+    ],
+
     'googlemap' => [
         'api_key' => env('GOOGLEMAP_API_KEY')
     ]
